0ff1088dc06902af532ce0702ae6e75371f2d382
John Brownlee <apps@johnbrownlee.com>
2014-09-06 13:29:37 -0400

    Initial Commit

---
+++ Tailor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tailor
+//
+//  Created by John Brownlee on 6/9/14.
+//  Copyright (c) 2014 John Brownlee. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
