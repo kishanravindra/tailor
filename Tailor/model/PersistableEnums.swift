7c25120de25438f09a519349bbce9779810123cb
John Brownlee <apps@mail.johnbrownlee.com>
2015-07-02 21:08:14 -0400

    Adds a protocol for automatically persisting enums. Issue #47

---
+++ Tailor/model/PersistableEnums.swift
@@ -0,0 +1,153 @@
+/**
+  This protocol is describes an enum that can be persisted in the database.
+
+  You shouldn't implement this protocol directly. Instead, you should implement
+  `StringPersistableEnum` or `TablePersistableEnum`.
+  */
+public protocol PersistableEnum: DatabaseValueConvertible {
+  /**
+    This method gets the name of a case of an enum.
+  
+    The default value uses the underscored version of the literal for the case.
+    */
+  var caseName: String { get }
+  
+  /** This method gets all of the cases in the enum. */
+  static var cases: [Self] { get }
+  
+  /**
+    This method gets an enum case from a database value.
+  
+    - parameter databaseValue:    The value from the database.
+    - returns                     The enum case
+    */
+  static func fromDatabaseValue(databaseValue: DatabaseValue) -> Self?
+}
+
+/**
+  This protocol describes an enum that can be persisted to a string value in
+  the database.
+
+  This can only be implemented by enum types.
+
+  The only method you need to implement for the protocol is the `cases`
+  method. The protocol provides the rest.
+  */
+public protocol StringPersistableEnum: PersistableEnum {
+}
+
+/**
+  This protocol describes an enum that can be persisted to an integer value
+  in the database, which will be mapped to a foreign key holding all the
+  possible enum cases.
+
+  The only method you need to implement for the protocol is the `cases`
+  method. The protocol provides the rest.
+
+  This requires that the table has two columns: `id` and `name`. `id` must be
+  an auto-incrementing primary key, and `name` must be a string column.
+  */
+public protocol TablePersistableEnum: PersistableEnum, ModelType {
+  /**
+    This method gets the name of the table that holds the cases for the enum.
+
+    The default implementation gets the pluralized version of the table name.
+    */
+  static var tableName: String { get }
+}
+
+public extension PersistableEnum {
+  var caseName: String {
+    let fullName = reflect(self).summary
+    let components = fullName.componentsSeparatedByString(".")
+    let caseName = components[components.count - 1]
+    return caseName.underscored()
+  }
+  
+  /**
+    This method creates an enum case from a case name.
+
+    - parameter caseName:   The name of the case, from the database.
+    - returns:              The case.
+    */
+  static func fromCaseName(caseName: String) -> Self? {
+    for item in self.cases {
+      if item.caseName == caseName {
+        return item
+      }
+    }
+    return nil
+  }
+}
+
+public extension StringPersistableEnum {
+  static func fromDatabaseValue(databaseValue: DatabaseValue) -> Self? {
+    if let caseName = databaseValue.stringValue {
+      return self.fromCaseName(caseName)
+    }
+    else {
+      return nil
+    }
+  }
+  
+  var databaseValue: DatabaseValue {
+    return self.caseName.databaseValue
+  }
+}
+
+public extension TablePersistableEnum {
+  static var tableName: String {
+    return self.modelName().pluralized
+  }
+  
+  /**
+    This method gets the case name that corresponds to an id.
+
+    This will fetch a record from the table by id, and return its `name`
+    parameter.
+
+    - parameter id:   The id to fetch
+    - returns:        The corresponding name.
+    */
+  static func caseNameForId(id: Int) -> String? {
+    let connection = Application.sharedDatabaseConnection()
+    var result = connection.executeQuery("SELECT * FROM \(tableName) WHERE id=?", id)
+    
+    if result.isEmpty { return nil }
+    let name = result[0].data["name"]
+    return name?.stringValue
+  }
+  
+  /**
+    This method gets an enum case from an id.
+  
+    This will get the case name for the id from the database and then get the
+    case whose name matches the case name.
+
+    - parameter id:   The id
+    - returns:        The case.
+    */
+  static func fromId(id: Int) -> Self? {
+    guard let name = self.caseNameForId(id) else { return nil }
+    return self.fromCaseName(name)
+  }
+  
+  static func fromDatabaseValue(databaseValue: DatabaseValue) -> Self? {
+    guard let id = databaseValue.intValue else { return nil }
+    return self.fromId(id)
+  }
+  
+  var databaseValue: DatabaseValue {
+    let connection = Application.sharedDatabaseConnection()
+    let tableName = self.dynamicType.tableName
+    let caseName = self.caseName
+    var result = connection.executeQuery("SELECT * FROM \(tableName) WHERE name=?", caseName)
+    if result.isEmpty {
+      result = connection.executeQuery("INSERT INTO \(tableName) (name) VALUES (?)", caseName)
+      if result.isEmpty {
+        return DatabaseValue.Null
+      }
+    }
+    return result[0].data["id"] ?? DatabaseValue.Null
+  }
+}
---
+++ TailorTests/model/PersistableEnumTests.swift
@@ -0,0 +1,89 @@
+import Tailor
+import TailorTesting
+
+class PersistableEnumTests: TailorTestCase {
+  enum Color: StringPersistableEnum {
+    case Red
+    case DarkBlue
+    
+    static var cases = [Color.Red, Color.DarkBlue]
+  }
+  
+  enum HatType: TablePersistableEnum {
+    case Feathered
+    case WideBrim
+    
+    static var cases = [HatType.Feathered, HatType.WideBrim]
+  }
+  
+  override func setUp() {
+    super.setUp()
+    Application.sharedDatabaseConnection().executeQuery("DROP TABLE IF EXISTS hat_types")
+    Application.sharedDatabaseConnection().executeQuery("CREATE TABLE hat_types (id integer PRIMARY KEY, name string)")
+    Application.sharedDatabaseConnection().executeQuery("INSERT INTO hat_types VALUES (1,'feathered')")
+  }
+  
+  override func tearDown() {
+    Application.sharedDatabaseConnection().executeQuery("DROP TABLE hat_types")
+  }
+  
+  func testStringPersistableEnumGetsStringFromFromCaseName() {
+    assert(Color.Red.databaseValue, equals: DatabaseValue.String("red"))
+    assert(Color.DarkBlue.databaseValue, equals: DatabaseValue.String("dark_blue"))
+  }
+  
+  func testStringPersistableEnumCanGenerateValueFromString() {
+    assert(Color.fromDatabaseValue(DatabaseValue.String("red")), equals: Color.Red)
+    assert(Color.fromDatabaseValue(DatabaseValue.String("dark_blue")), equals: Color.DarkBlue)
+  }
+  
+  func testStringPersistableEnumGetsNilForInvalidCaseName() {
+    assert(isNil: Color.fromDatabaseValue(DatabaseValue.String("blue")))
+  }
+  
+  func testStringPersistableEnumGetsNilForIntegerValue() {
+    assert(isNil: Color.fromDatabaseValue(DatabaseValue.Integer(5)))
+  }
+  
+  func testDatabasePersistableEnumRecognizesExistingRecord() {
+    let value = HatType.Feathered
+    assert(value.databaseValue, equals: DatabaseValue.Integer(1))
+  }
+  
+  func testDatabasePersistableEnumCreatesNewRecord() {
+    let value = HatType.WideBrim
+    assert(value.databaseValue, equals: DatabaseValue.Integer(2))
+    let value2 = value
+    assert(value2.databaseValue, equals: DatabaseValue.Integer(2))
+  }
+  
+  func testDatabasePersistableEnumWithProblemInsertingRecordReturnsNull() {
+    Application.sharedDatabaseConnection().executeQuery("DROP TABLE hat_types")
+    Application.sharedDatabaseConnection().executeQuery("CREATE TABLE hat_types (id integer PRIMARY KEY, name string CHECK(length(name)>10))")
+    let value = HatType.Feathered
+    assert(value.databaseValue, equals: DatabaseValue.Null)
+  }
+  
+  func testDatabasePersistableEnumWithBadStructureReturnsNull() {
+    Application.sharedDatabaseConnection().executeQuery("DROP TABLE hat_types")
+    Application.sharedDatabaseConnection().executeQuery("CREATE TABLE hat_types (id integer PRIMARY KEY")
+    let value = HatType.Feathered
+    assert(value.databaseValue, equals: DatabaseValue.Null)
+  }
+  
+  func testDatabasePersistableEnumCanGenerateValueFromId() {
+    let value1 = HatType.Feathered.databaseValue
+    let value2 = HatType.WideBrim.databaseValue
+    
+    assert(HatType.fromDatabaseValue(value1), equals: HatType.Feathered)
+    assert(HatType.fromDatabaseValue(value2), equals: HatType.WideBrim)
+  }
+  
+  func testDatabasePersistableEnumGetsNilForInvalidId() {
+    _ = HatType.Feathered.databaseValue
+    _ = HatType.WideBrim.databaseValue
+    
+    assert(HatType.fromDatabaseValue(3.databaseValue) == nil)
+  }
+
+}
