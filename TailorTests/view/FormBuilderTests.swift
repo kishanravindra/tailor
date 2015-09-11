8d59eb2f8f07c7c82c81fc717ea8fc9dae4133cc
John Brownlee <apps@mail.johnbrownlee.com>
2015-09-11 15:44:48 -0400

    Fixes some missing content in a test.

--- TailorTests/view/FormBuilderTests.swift
+++ TailorTests/view/FormBuilderTests.swift
@@ -14,6 +14,7 @@ import TailorTesting
   
   override func setUp() {
     super.setUp()
+    _ = Application()
     let controller = TestController(request: Request(), response: Response(), actionName: "index", callback: {response in })
     builder = FormBuilder(template: Template(controller: controller), name: "hat")
   }
